a3abc13e814b587facd533f6bd48963accaff1a5
qiandongzhao <987889957@qq.com>
2022-09-22 21:24:03 +0800

    commit

--- app/src/main/java/com/dryghsfzhaocaimao/regfhseradfert/zhaocaimaowegapi/RetrofitZhaoCaiCatKfrtManager.java
+++ app/src/main/java/com/dryghsfzhaocaimao/regfhseradfert/zhaocaimaowegapi/RetrofitZhaoCaiCatKfrtManager.java
@@ -18,8 +18,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitZhaoCaiCatKfrtManager {
 
-    public static final String ZCXY = "https://gnxys.pycxwl.cn/profile/opzcmdk/zcxy.html";
-    public static final String YSXY = "https://gnxys.pycxwl.cn/profile/opzcmdk/ysxy.html";
+    public static final String ZCXY = "https://xyssml.yiqian888.xyz/profile/opzcmdk/zcxy.html";
+    public static final String YSXY = "https://xyssml.yiqian888.xyz/profile/opzcmdk/ysxy.html";
     public static final String API_BASE_URL = "http://117.50.185.81:7743/";
 
     private volatile static RetrofitZhaoCaiCatKfrtManager retrofitZhaoCaiCatKfrtManager;
