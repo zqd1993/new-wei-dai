d015bb0f67dd4a5fc60eaf8383c427438c91d1cc
qiandongzhao <987889957@qq.com>
2022-10-12 17:02:39 +0800

    commit

--- app/src/main/java/com/youjiegherh/pocketqwrh/youjiewetdfhui/YouJieSDjdfiGoodsDetailsActivity.java
+++ app/src/main/java/com/youjiegherh/pocketqwrh/youjiewetdfhui/YouJieSDjdfiGoodsDetailsActivity.java
@@ -209,6 +209,14 @@ public class YouJieSDjdfiGoodsDetailsActivity extends BaseNewCodeXiaoNiuKuaiActi
         }
     }
 
+    private String setFilePath() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Environment.isExternalStorageLegacy()) {
+            return this.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/apk";
+        }
+        String packageName = getApplicationContext().getPackageName();
+        return filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + packageName;
+    }
+
     public void downFile(String url) {
         ProgressDialog progressDialog = new ProgressDialog(YouJieSDjdfiGoodsDetailsActivity.this);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -218,7 +226,7 @@ public class YouJieSDjdfiGoodsDetailsActivity extends BaseNewCodeXiaoNiuKuaiActi
         progressDialog.show();
         progressDialog.setCancelable(false);
         String apkName[] = url.split("/");
-        YouJieSDjdfiDownloadApkUtil.get().download(url, Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/", apkName[apkName.length - 1], new YouJieSDjdfiDownloadApkUtil.OnDownloadListener() {
+        YouJieSDjdfiDownloadApkUtil.get().download(url, setFilePath(), apkName[apkName.length - 1], new YouJieSDjdfiDownloadApkUtil.OnDownloadListener() {
             @Override
             public void onDownloadSuccess(File file) {
                 if (progressDialog != null && progressDialog.isShowing()) {
