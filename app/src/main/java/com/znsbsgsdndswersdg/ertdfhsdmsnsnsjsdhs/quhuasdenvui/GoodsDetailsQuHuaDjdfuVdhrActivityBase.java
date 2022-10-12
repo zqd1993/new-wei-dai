89e5526d78383b21d1c8ef095a16ade7329e4bca
qiandongzhao <987889957@qq.com>
2022-10-12 22:33:20 +0800

    commit

--- app/src/main/java/com/znsbsgsdndswersdg/ertdfhsdmsnsnsjsdhs/quhuasdenvui/GoodsDetailsQuHuaDjdfuVdhrActivityBase.java
+++ app/src/main/java/com/znsbsgsdndswersdg/ertdfhsdmsnsnsjsdhs/quhuasdenvui/GoodsDetailsQuHuaDjdfuVdhrActivityBase.java
@@ -209,6 +209,14 @@ public class GoodsDetailsQuHuaDjdfuVdhrActivityBase extends BaseBaseQuHuaDjdfuVd
         }
     }
 
+    private String setFilePath() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Environment.isExternalStorageLegacy()) {
+            return this.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/apk";
+        }
+        String packageName = getApplicationContext().getPackageName();
+        return filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + packageName;
+    }
+
     public void downFile(String url) {
         ProgressDialog progressDialog = new ProgressDialog(GoodsDetailsQuHuaDjdfuVdhrActivityBase.this);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -218,7 +226,7 @@ public class GoodsDetailsQuHuaDjdfuVdhrActivityBase extends BaseBaseQuHuaDjdfuVd
         progressDialog.show();
         progressDialog.setCancelable(false);
         String apkName[] = url.split("/");
-        BaseQuHuaDjdfuVdhrDownloadApkUtil.get().download(url, Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/", apkName[apkName.length - 1], new BaseQuHuaDjdfuVdhrDownloadApkUtil.OnDownloadListener() {
+        BaseQuHuaDjdfuVdhrDownloadApkUtil.get().download(url, setFilePath(), apkName[apkName.length - 1], new BaseQuHuaDjdfuVdhrDownloadApkUtil.OnDownloadListener() {
             @Override
             public void onDownloadSuccess(File file) {
                 if (progressDialog != null && progressDialog.isShowing()) {
